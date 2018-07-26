6e202baabdd0f35f42ad31c0af586f0b1094ca74
MIClavelTe <miclavelte@gmail.com>
2018-07-26 16:31:58 -0700

    Random prompt & hint

--- app.js
+++ app.js
@@ -10,7 +10,13 @@ app.get('/', (req,res) => {
 });
 
 app.get('/cards', (req,res) => {
-    res.render('cards', {prompt: "What's the smallest animal on Earth?"});
+    const questions = ["What's the smallest animal on Earth?", "How many legs does a milipede have?",
+     "How big is a baby kangaroo?", "What's the loudest animal in the world?", "How fast can a cheetah run?",
+    "What's the longest living animal?"]
+    const hints = ["It lives in the ocean", "Number from 100 - 1000", "Less than 1 foot", "It can't walk or fly",
+    "Number from 10 - 200", "It's a warm-blooded mammal"]
+    var random = Math.floor(Math.random() * 10) + 1
+    res.render('cards', {prompt: questions[random], hint: hints[random]});
 });
 
 app.get('/student', (req,res) => {

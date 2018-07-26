3e1026f2a7269831a29da4e6064c43af41c59460
MIClavelTe <miclavelte@gmail.com>
2018-07-26 10:52:15 -0700

    Render views

--- app.js
+++ app.js
@@ -2,3 +2,15 @@ const express = require('express');
 const app = express()
 
 app.set('view engine', 'pug');
+
+app.get('/', (req,res) => {
+    res.render('index');
+});
+
+app.get('/cards', (req,res) => {
+    res.render('cards', {prompt: "What's the smallest animal on Earth?", hint: "The animal lives deep under the ocean"});
+});
+
+app.listen(4000, () => {
+    console.log('Server Running at localhost:4000');
+});

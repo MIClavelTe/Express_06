0bfef23eb83dfb46b3e77c4e0db3b3162a331112
MIClavelTe <miclavelte@gmail.com>
2018-07-26 18:43:03 -0700

    Add post request

--- app.js
+++ app.js
@@ -18,6 +18,15 @@ app.get('/cards', (req,res) => {
     var random = Math.floor(Math.random() * 10) + 1
     res.render('cards', {prompt: questions[random], hint: hints[random]});
 });
+app.post('/cards', (req,res) => {
+    const questions = ["What's the smallest animal on Earth?", "How many legs does a milipede have?",
+     "How big is a baby kangaroo?", "What's the loudest animal in the world?", "How fast can a cheetah run?",
+    "What's the longest living animal?"]
+    const hints = ["It lives in the ocean", "Number from 100 - 1000", "Less than 1 foot", "It can't walk or fly",
+    "Number from 10 - 200", "It's a warm-blooded mammal"]
+    var random = Math.floor(Math.random() * 10) + 1
+    res.render('cards', {prompt: questions[random], hint: hints[random]});
+});
 
 app.get('/student', (req,res) => {
     res.render('student');

094aed2d622ded599a2c288d0b0cd6913689c849
MIClavelTe <miclavelte@gmail.com>
2018-07-27 20:23:39 -0700

    Add answers

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const questions = ["What's the smallest animal on Earth?", "How many legs does a
     "How many years old can a mouse be?"]
 const hints = ["It lives in the ocean", "Number from 100 - 1000", "Less than 1 foot", "It can't walk or fly",
     "Number from 10 - 200", "It's a warm-blooded mammal", "Number from 1 - 50", "Number from 1 - 30"]
-const answers = ['Water Bear', "400", "2 cm", ]
+const answers = ['Water Bear', "400", "2 cm", "Sperm Whale", "70 mph", "Bowhead Whale", "20 times", "2 years"]
 
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(cookieParser());
@@ -21,11 +21,11 @@ app.get('/', (req,res) => {
 
 app.get('/cards', (req,res) => {
     var random = Math.floor(Math.random() * 10) + 1
-    res.render('cards', {prompt: questions[random], hint: hints[random]});
+    res.render('cards', {prompt: questions[random], hint: hints[random], answer: answers[random]});
 });
 app.post('/cards', (req,res) => {
     var random = Math.floor(Math.random() * 10) + 1
-    res.render('cards', {prompt: questions[random], hint: hints[random]});
+    res.render('cards', {prompt: questions[random], hint: hints[random], answer: answers[random]});
 });
 
 app.get('/student', (req,res) => {

9fd5f5e2d83fd200a74300662d18bb70ae27a35d
MIClavelTe <miclavelte@gmail.com>
2018-08-01 14:40:57 -0700

    Add flashcard data

--- routes/cards.js
+++ routes/cards.js
@@ -1,21 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
-const questions = ["What's the smallest animal on Earth?", "How many legs does a milipede have?",
-     "How big is a baby kangaroo?", "What's the loudest animal in the world?", "How fast can a cheetah run?",
-    "What's the longest living animal?", "How many times can a woodpecker peck per sec?", 
-    "How many years old can a mouse be?"]
-const hints = ["It lives in the ocean", "Number from 100 - 1000", "Less than 1 foot", "It can't walk or fly",
-    "Number from 10 - 200", "It's a warm-blooded mammal", "Number from 1 - 50", "Number from 1 - 30"]
-const answers = ['Water Bear', "400", "2 cm", "Sperm Whale", "70 mph", "Bowhead Whale", "20 times", "2 years"]
+const { data } = require('../data/flashcard.json');
+const { cards } = data;
 
 router.get('/', (req,res) => {
-    var random = Math.floor(Math.random() * 10) + 1
-    res.render('cards', {prompt: questions[random], hint: hints[random], answer: answers[random]});
+    var random = Math.floor(Math.random() * 10)
+    res.render('cards', {
+        prompt: cards[random].question, 
+        hint: cards[random].hint, 
+        answer: cards[random].answer
+    });
 });
 router.post('/', (req,res) => {
-    var random = Math.floor(Math.random() * 10) + 1
-    res.render('cards', {prompt: questions[random], hint: hints[random], answer: answers[random]});
+    var random = Math.floor(Math.random() * 10)
+    res.render('cards', {
+        prompt: cards[random].question, 
+        hint: cards[random].hint, 
+        answer: cards[random].answer
+    });
 });
 
 module.exports = router;
